7bfbab4f26232404a2cf53f049f162bd1b27c739
FullTimeScam <418.gingin@gmail.com>
2024-05-30 16:57:49 +0900

    여러 사진, 메타데이터가 있는 NFT 발행하기

---
+++ MinNFT_bulk.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+
+contract MintNFTBulk is ERC721Enumerable {
+    string metadataUri;
+
+    constructor( string memory _name, string memory _symbol, string memory _metadataUri) ERC721(_name, _symbol) {
+        metadataUri=_metadataUri;
+    }
+
+    function mintNFT_BULK() public {
+        require(totalSupply()<9, "No more mint");
+
+        uint tokenId = totalSupply() +1;
+
+        _mint(msg.sender, tokenId);
+    }
+
+    function tokenURI(uint _tokenId) public view override returns (string memory){
+        return string(abi.encodePacked(metadataUri, Strings.toString(_tokenId), ".json"));
+
+    }
+}
