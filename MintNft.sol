dde8246ca5a30181c9db00fae1df87d1006c1807
FullTimeScam <418.gingin@gmail.com>
2024-05-30 14:35:15 +0900

    쉼표 하나 잘못 찍어서 재업

--- MintNft.sol
+++ MintNft.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
 
 contract MintNft is ERC721Enumerable, Ownable {
     mapping(uint => string) metadataUri;
 
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol), Ownable(msg.sender) {}
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) Ownable(msg.sender) {}
 
     function mintNft(string memory _metadataUri) public payable {
         require(msg.value >= 0.01 ether, "Not enough ETH");
